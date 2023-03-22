fa5edef52c358bfabdda7fc34d7dc9a229e33034
melvinjames <melvin.gacheru@student.morigaschool.com>
2023-03-22 12:26:26 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(start){
   // Write your code here!
+  let result;
+  if(start<=400){
+   return result ='this one is on me!';
 }
+  else if (start<2499){
+      return result = 'i will gladly take your thirty bucks.';
+  }
+      else (start > 2501)
+       return result = 'no can do';
+}
+scuberGreetingForFeet(start)
 
-function ternaryCheckCity(){
+function ternaryCheckCity(start){
   // Write your code here!
+let result;
+if (start === 'NYC'){
+  return result = 'Ok, sounds good.';
 }
+else {
+  return result = 'No Go.';
+}}
+ternaryCheckCity('Pittsburgh')
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(start){
+  // Write your code here!let result;
+  let result;
+  if (start === 'generous'){
+    return result = 'Thank you so much.';
+  }
+  else if (start === 'not as generous'){
+    return result = 'Thank you.'
+  }
+  else {
+    return result ='Bye.';
+  }
 }
+switchOnCharmFromTip(generous)
+
